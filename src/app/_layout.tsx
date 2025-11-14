46f2fa0a4458510a2fcf4fa929c17b1551cbb1cc
pasmarques <pedroaffonsosilvamarques@gmail.com>
2025-11-14 15:00:37 -0300

    feat: expo router and nativewind with sdk 54

---
+++ src/app/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from "expo-router";
+import "../../global.css";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Stack
+        screenOptions={{
+          headerShown: false,
+        }}
+      />
+    </SafeAreaProvider>
+  );
+}
---
+++ src/app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function App() {
+  return (
+    <SafeAreaView className="flex-1 bg-white">
+      <View className="flex-1 items-center justify-center">
+        <Text className="text-xl font-bold text-blue-500">
+          Welcome to Nativewind!
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
+}
